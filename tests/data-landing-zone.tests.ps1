dcd83f38c2cfe2fa3b935f1ed58a3b98b63e27a5
Jesse Wellenberg <jesse.wellenberg@axians.com>
2022-07-05 14:19:17 +0200

    Add example integrationtest

---
+++ tests/data-landing-zone.tests.ps1
@@ -0,0 +1,18 @@
+
+
+Describe "Data Landing Zone Tests" {
+    BeforeAll {
+        $paramJson = Get-Content "./parameters.json" | Out-String | ConvertFrom-Json
+        $project = $paramJson.parameters.project.value
+        $dlzTags = $paramJson.parameters.dlzTags.value
+        $dlzLocation = $paramJson.parameters.dlzLocation.value
+        $dlzAddressSubspace = $paramJson.parameters.dlzLocation.value;
+    }
+
+    It "It should create a resource group" {
+        $rgName = "rg-${project}"
+        $rg = Get-AzResourceGroup -Name $rgName 
+
+        $rg.ResourceGroupName | Should -Be $rgName
+    }
+}
