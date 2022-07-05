10dbb01aca2426017b59715d14aa6ac6fb7f3d9f
Jesse Wellenberg <jesse.wellenberg@axians.com>
2022-07-05 15:00:28 +0200

    Add storage account test

--- tests/data-landing-zone.tests.ps1
+++ tests/data-landing-zone.tests.ps1
@@ -4,14 +4,26 @@ Describe "Data Landing Zone Tests" {
         $project = $paramJson.parameters.project.value
         $dlzTags = $paramJson.parameters.dlzTags.value
         $dlzLocation = $paramJson.parameters.dlzLocation.value
-        $dlzAddressSubspace = $paramJson.parameters.dlzLocation.value;
+        $dlzAddressSubspace = $paramJson.parameters.dlzLocation.value
+        $rgName = "rg-${project}"
     }
 
     It "It should create a resource group" {
-        $rgName = "rg-${project}"
-
         $rg = Get-AzResourceGroup -Name $rgName 
 
         $rg.ResourceGroupName | Should -Be $rgName
+    }
+
+    It "It should create a storage account" {
+        $trimmedProj = $project.Replace("-", "")
+        $expAccountName = "dls${trimmedProj}"
+        $rgName 
+        $expAccountName
+        $account = Get-AzStorageAccount -ResourceGroupName $rgName -Name $expAccountName
+
+        $account.StorageAccountName | Should -Be $expAccountName
+        $account.Sku.Name | Should -Be 'Standard_LRS'
+        $account.Kind | Should -Be 'StorageV2'
+        $account.Location | Should -Be $dlzLocation
     }
 }
